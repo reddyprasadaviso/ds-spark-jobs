2254f8713e120cb19ab07a7cbfcf59de65e6f15f
reddyprasadaviso <reddy.prasad@aviso.com>
2025-10-03 11:07:31 +0530

    updated the spark-jobs

---
+++ pi.py
@@ -0,0 +1,15 @@
+# pi.py
+from __future__ import print_function
+from random import random
+from operator import add
+from pyspark.sql import SparkSession
+
+if __name__ == "__main__":
+    spark = SparkSession.builder.appName("PythonPi").getOrCreate()
+    n = 100000
+    count = spark.sparkContext.parallelize(range(1, n + 1)).map(
+        lambda _: 1 if random()**2 + random()**2 < 1 else 0
+    ).reduce(add)
+    print("Pi is roughly %f" % (4.0 * count / n))
+    spark.stop()
+
